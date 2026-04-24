0cae45742866fd7aae14e4532187b8b183eef801
bashar al-shayyah <basharsh.191@gmail.com>
2026-04-24 15:30:03 +0300

    add translet end

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function store(Request $request)
+public function store(Request $request)
 {
     $validated = $request->validate([
         'customer_name' => 'required|string',
@@ -28,9 +28,10 @@ public function update(Request $request, Order $order)
 
     return response()->json(['message' => 'Order updated successfully', 'order' => $order], 200);
 }
-    public function destroy(Order $order)
+public function destroy(Order $order)
     {
         $order->delete();
         return response()->json(['message'=>'oreder is deleted'], 200);
-    }
+}
+//
 }
